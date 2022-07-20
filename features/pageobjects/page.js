43d4d49b2b8804db34d535a41231417095331a6b
afifahayumi <afifah.ayumi@gmail.com>
2022-07-20 10:57:29 +0700

    change result selector

--- features/pageobjects/page.js
+++ features/pageobjects/page.js
@@ -3,9 +3,8 @@ class homePage
     //selector
     get btnSearch(){return $('/html/body/header/div/div[2]/div/div[2]/div/button')}
     get txtSearch(){return $('//*[@id="search-input--mobile"]')}
-    get result(){return $("//*[@href = 'https://www.verint.com/press-room/2019-press-releases/verint-voice-of-customer-solutions-recognized-among-top-digital-voice-of-customer-voc-specialists-by-independent-research-firm/' and (text() = 'Verint Voice of Customer Solutions Recognized Among Top Digital Voice of Customer (VoC) Specialists by Independent Research Firm' or . = 'Verint Voice of Customer Solutions Recognized Among Top Digital Voice of Customer (VoC) Specialists by Independent Research Firm')]")}
-
-    //method
+    //get result(){return $("//*[@href = 'https://www.verint.com/press-room/2019-press-releases/verint-voice-of-customer-solutions-recognized-among-top-digital-voice-of-customer-voc-specialists-by-independent-research-firm/' and (text() = 'Verint Voice of Customer Solutions Recognized Among Top Digital Voice of Customer (VoC) Specialists by Independent Research Firm' or . = 'Verint Voice of Customer Solutions Recognized Among Top Digital Voice of Customer (VoC) Specialists by Independent Research Firm')]")}
+    get result(){return $("//header[@id='site-header']/div/div[2]/div/div[2]/div/div/div/div/div[2]")}
         open(){
         return browser.url(`https://www.verint.com/`)}
 
